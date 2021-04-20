15cdb167c298dcaeef2ffd4020dffce909272c31
Laurent Labine <laurentlabine@gmail.com>
2021-04-20 14:37:50 +0200

    Refactored App to allow myself a decent layout.
    Also Limited digits display and added a limit reached message.

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import GlobalStyle from './styles'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import GlobalStyle from './styles';
 
-const numberToWords = require('number-to-words')
+const numberToWords = require('number-to-words');
 
 const inputs = {
   zero: '0',
@@ -14,186 +14,249 @@ const inputs = {
   six: '6',
   seven: '7',
   eight: '8',
-  nine: '9'
-}
+  nine: '9',
+};
 
 const operators = {
   add: '+',
   subtract: '-',
   multiply: '*',
-  divide: '/'
-}
+  divide: '/',
+};
 
 const Input = (props) => {
   return (
     <button id={props.id} onClick={props.handler}>
       {props.operation}
     </button>
-  )
-}
+  );
+};
 
 Input.propTypes = {
   id: PropTypes.string,
   handler: PropTypes.string,
-  operation: PropTypes.string
-}
+  operation: PropTypes.string,
+};
+
+const Digit = (props) => {
+  return (
+    <button
+      className="grid-item"
+      key={numberToWords.toWords(props.value)}
+      id={numberToWords.toWords(props.value)}
+      operation={props.value}
+      onClick={props.handler}
+    >
+      {props.value}
+    </button>
+  );
+};
+
+Digit.propTypes = {
+  value: PropTypes.number,
+  handler: PropTypes.func,
+};
+
+const Operator = (props) => {
+  return (
+    <button
+      className="grid-item"
+      id={props.id}
+      operation={props.operation}
+      onClick={props.handler}
+    >
+      {props.operation}
+    </button>
+  );
+};
+
+Operator.propTypes = {
+  id: PropTypes.string,
+  operation: PropTypes.string,
+  handler: PropTypes.func,
+};
 
 class Calculator extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      x: '',
-      data: [],
-      display: '',
-      res: 0
-    }
+      x: '', // Current Number Being treated
+      data: [], //
+      display: '', // displayed Input
+    };
 
-    this.handleInput = this.handleInput.bind(this)
+    this.handleInput = this.handleInput.bind(this);
   }
 
-  handleInput (e) {
-    const input = e.target.id
-    let str = ''
-    let res = ''
-    let arr = []
-    const foo = eval
+  limitReached() {
+    let prevMsg = '';
+    if (!this.state.display.startsWith('Limit Reached')) {
+      prevMsg = this.state.display;
+      this.setState({
+        display: 'Limit Reached',
+      });
+      setTimeout(
+        function () {
+          // Start the timer
+          this.setState({ display: prevMsg }); // After 1 second, set render to true
+        }.bind(this),
+        1000
+      );
+    }
+  }
 
+  handleInput(e) {
+    const input = e.target.id;
+    let str = '';
+    let res = '';
+    let arr = [];
+
+    const evaluate = eval;
     switch (input) {
       case 'equals':
         str = this.filterResult(this.state.data.concat(this.state.x))
           .join('')
-          .replace(/,/g, '')
-        res = foo(str)
-        arr = [res]
+          .replace(/,/g, '');
+        res = evaluate(str);
+        console.log('RES : ' + res);
+
+        arr = [res];
         this.setState({
           display: res,
           data: arr,
-          x: ''
-        })
+          x: '',
+        });
 
-        break
+        break;
       case 'clear':
         this.setState({
           x: '',
           data: [],
-          display: '0'
-        })
-        break
+          display: '0',
+        });
+        break;
       case 'decimal':
-        if (!this.state.x.includes('.')) {
-          this.setState({
-            x: this.state.x + '.',
-            display: this.state.display + '.'
-          })
+        if (this.state.display.length < 14) {
+          if (!this.state.x.includes('.')) {
+            this.setState({
+              x: this.state.x + '.',
+              display: this.state.display + '.',
+            });
+          }
+        } else {
+          this.limitReached();
         }
-        break
+        break;
       default:
         if (inputs[input]) {
-          this.handleDigit(input)
+          if (this.state.display.length < 14) {
+            this.handleDigit(input);
+          } else {
+            this.limitReached();
+          }
         } else {
-          this.handleOperator(operators[input])
+          this.handleOperator(operators[input]);
         }
-        break
+
+        break;
     }
   }
 
   // If 2 or more operators are entered consecutively, the operation performed
   // should be the last operator entered (excluding the negative (-) sign.
 
-  filterResult (inpArr) {
-    let tmpOprArr = []
-    const resArr = []
+  filterResult(inpArr) {
+    let tmpOprArr = [];
+    const resArr = [];
 
     for (let i = 0; i < inpArr.length; i++) {
-      if (inpArr[i] === '') continue
+      if (inpArr[i] === '') continue;
 
       if (isNaN(inpArr[i])) {
         // if it's a symbol, filter to a temporary array
-        tmpOprArr.push(inpArr[i])
-      } else { // If a number is found, filter the opr array to remove the unwanted operators
+        tmpOprArr.push(inpArr[i]);
+      } else {
+        // If a number is found, filter the opr array to remove the unwanted operators
         if (tmpOprArr.length > 1 && tmpOprArr.slice(-1).toString() === '-') {
           // last operator is a negative
-          resArr.push(tmpOprArr.slice(-2))
-          tmpOprArr = []
+          resArr.push(tmpOprArr.slice(-2));
+          tmpOprArr = [];
         } else if (tmpOprArr.length > 1) {
-          resArr.push(tmpOprArr.slice(-1))
-          tmpOprArr = []
+          resArr.push(tmpOprArr.slice(-1));
+          tmpOprArr = [];
         } else {
-          resArr.push(tmpOprArr)
-          tmpOprArr = []
+          resArr.push(tmpOprArr);
+          tmpOprArr = [];
         }
-        resArr.push(inpArr[i])
+        resArr.push(inpArr[i]);
       }
     }
-    return resArr
+    return resArr;
   }
 
-  setTest () {
+  setTest() {
     this.setState({
-      data: ['5', '*', '-', '+', '5']
-    })
+      data: ['5', '*', '-', '+', '5'],
+    });
   }
 
-  handleDigit (digit) {
+  handleDigit(digit) {
     if (this.state.display === '0') {
       this.setState({
         x: inputs[digit],
-        display: inputs[digit]
-      })
+        display: inputs[digit],
+      });
     } else {
       this.setState({
         x: this.state.x + inputs[digit],
-        display: this.state.display + inputs[digit]
-      })
+        display: this.state.display + inputs[digit],
+      });
     }
   }
 
-  handleOperator (operator) {
-    const tmpArr = this.state.data
+  handleOperator(operator) {
+    const tmpArr = this.state.data;
 
-    if (this.state.x !== '') tmpArr.push(this.state.x)
+    if (this.state.x !== '') tmpArr.push(this.state.x);
 
-    tmpArr.push(operator)
+    tmpArr.push(operator);
     this.setState({
       data: tmpArr,
-      display: this.state.display + operator,
-      x: ''
-    })
+      display: operator,
+      x: '',
+    });
   }
 
-  render () {
-    const pad = []
-
-    for (let i = 0; i < 10; i++) {
-      pad.push(
-        <Input
-          key={numberToWords.toWords(i)}
-          id={numberToWords.toWords(i)}
-          operation={i}
-          handler={this.handleInput}
-        >
-          {i}
-        </Input>
-      )
-    }
+  render() {
     return (
-      <React.Fragment>
+      <div id="calculator">
         <GlobalStyle />
-        {/* <div id='calculator'> */}
-        <div id='display'>{this.state.display}</div>
-        <Input id='equals' operation='=' handler={this.handleInput} />
-        {pad}
-        <Input id='add' operation='+' handler={this.handleInput} />
-        <Input id='subtract' operation='-' handler={this.handleInput} />
-        <Input id='multiply' operation='*' handler={this.handleInput} />
-        <Input id='divide' operation='/' handler={this.handleInput} />
-        <Input id='decimal' operation='.' handler={this.handleInput} />
-        <Input id='clear' operation='CL' handler={this.handleInput} />
+        <div id="outer">
+          <div id="display" className="grid-item">
+            <p>{this.state.display}</p>
+          </div>
+          <Operator id="clear" operation="CL" handler={this.handleInput} />
+          <Operator id="divide" operation="/" handler={this.handleInput} />
+          <Operator id="multiply" operation="*" handler={this.handleInput} />
+          <Digit value={7} handler={this.handleInput} />
+          <Digit value={8} handler={this.handleInput} />
+          <Digit value={9} handler={this.handleInput} />
+          <Operator id="subtract" operation="-" handler={this.handleInput} />
+          <Digit value={4} handler={this.handleInput} />
+          <Digit value={5} handler={this.handleInput} />
+          <Digit value={6} handler={this.handleInput} />
+          <Operator id="add" operation="+" handler={this.handleInput} />
+          <Digit value={1} handler={this.handleInput} />
+          <Digit value={2} handler={this.handleInput} />
+          <Digit value={3} handler={this.handleInput} />
+          <Operator id="equals" operation="=" handler={this.handleInput} />
+          <Digit value={0} handler={this.handleInput} />
+          <Operator id="decimal" operation="." handler={this.handleInput} />
+        </div>
         <br />
-        {/* </div> */}
-      </React.Fragment>
-    )
+      </div>
+    );
   }
 }
 
-export default Calculator
+export default Calculator;
--- src/calculator/styles.jsx
+++ src/calculator/styles.jsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components/macro'
+import { createGlobalStyle } from 'styled-components/macro';
 
 export default createGlobalStyle`
     #root {
@@ -16,16 +16,72 @@ export default createGlobalStyle`
     }
 
     body {
-        background-image: url("https://user-images.githubusercontent.com/20836468/115224640-67059d00-a10d-11eb-8a10-9d799e4fc93d.jpg");
+        background-position: center;
+        background-image: url("https://user-images.githubusercontent.com/20836468/115396602-7526ec80-a1e5-11eb-83b6-19ce0920105b.jpg");
+        background-position: center;
+    }
+
+    #calculator {
+        display: flex;
+        position: absolute;
+        padding: 10px;
+        height: 22rem;
+        width: 20rem;
+        border-radius: 5px;
+        border: 2px solid white;
+        text-align: center;
+        background-image: url("https://user-images.githubusercontent.com/20836468/115257250-2ec38600-a130-11eb-8964-ce296652a0b1.jpg");
         background-size: 100%;
         background-position: center;
     }
 
-    #controls {
-        color: white;
-        position: relative;
-        bottom: 240px;
-        display: float;
-        float: right;
+    #display {
+        grid-column : 1/5;
+        font-family: 'Bubblegum Sans', cursive;
+        font-size: 25px;
+        height: 5rem;
+        background-color: #1D16A0;
+        color: green;
     }
-    `
+
+    #display p{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+
+
+    #outer {
+        display: grid;
+        grid-template-columns: auto auto auto auto;
+        grid-gap: 10px;
+        position: relative;
+        left:0;
+        right:0;
+        width:100%;
+        height: 75%;
+    }
+
+    .grid-item {
+        background-color: rgba(255, 255, 255, 0.8);
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        height: 100%;
+        font-size: 30px;
+        text-align: center;
+        vertical-align: center;
+    }
+
+    #clear {
+        grid-column: 1 / 3;
+    }
+
+    #equals {
+        grid-row: 5/7;
+        grid-column: 4;
+    }
+
+    #zero {
+        grid-row: 6/7;
+        grid-column: 1/3;
+    }
+    `;
